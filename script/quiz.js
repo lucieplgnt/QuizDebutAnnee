274241643802f22e34587d9cb0f9ddb04e2a5c91
Lucie <75979787+lucieplgnt@users.noreply.github.com>
2023-10-24 15:51:16 +0200

    Add files via upload

---
+++ script/quiz.js
@@ -0,0 +1,19 @@
+function score(){
+    let q1 = document.question1.choix[0].checked;
+    let q2 = document.question2.choix[0].checked;
+    let q3 = document.question3.choix[0].checked;
+    return q1 + q2 + q3
+}
+
+function goToReponse() {
+    let score = this.score();
+    if(score > 1) {
+        window.location.href = "/TestDebutAnnee/reponseOK.html"
+    }else {
+        window.location.href = "/TestDebutAnnee/reponseKO.html"
+    }
+}
+
+function goToQuiz(){
+    window.location.href = "/TestDebutAnnee/quiz.html"
+}
